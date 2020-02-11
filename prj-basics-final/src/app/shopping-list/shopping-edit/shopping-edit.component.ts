129b30d8d8784b4ed13bcba6b58b495720b58b76
Ryan Marinoff <ryan.marinoff@gmail.com>
2020-02-11 12:27:26 -0500

    Finished this part of the project, onto next lesson

--- prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() newIngredient: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput', { static: false }) nameInput: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  add() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+    this.newIngredient.emit(new Ingredient(name, amount));
+  }
+
+  delete() {
+
+  }
+
+  clear() {
+
+  }
 }
